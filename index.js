45be13f72dcc0f52bd5b0938726b259dc5263705
Brian Steere <dianoga7@3dgo.net>
2018-10-01 11:55:04 -0500

    Send back response from wundergroun. Also do date ourselves

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const fetch = require('node-fetch');
+const querystring = require('querystring');
+const moment = require('moment');
 
 const port = 3000;
 const stUrl = process.env.ST_URL;
@@ -9,9 +11,25 @@ const app = express();
 app.get('*', async (req, res) => {
 	// Update wunderground
 	try {
-		const wuUrl = `http://${req.hostname}${req.url}`;
+		const data = req.query;
+		data.dateutc = moment.utc().format('YYYY-MM-DD HH:mm:ss');
+		const wuUrl = `http://${req.hostname}${req.path}?${querystring.stringify(
+			data
+		)}`;
 		const wuResp = await fetch(wuUrl);
-		console.log('WU update success');
+
+		const respBody = await wuResp.text();
+
+		wuResp.headers.forEach((val, key) => {
+			res.set(key, val);
+		});
+		res.send(respBody);
+
+		if (wuResp.ok) {
+			console.log('WU update success');
+		} else {
+			console.warn(`WU update error (${wuResp.status})`, respBody);
+		}
 	} catch (e) {
 		console.error('WU Update failed', e);
 	}
