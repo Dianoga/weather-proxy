4de4d58bc248dd0dd43161239608ec96afcb64a2
Brian Steere <dianoga7@3dgo.net>
2020-02-02 14:43:27 -0600

    Switch to polling the weather station for updates

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const express = require('express');
 const fetch = require('node-fetch');
+const cheerio = require('cheerio');
 const querystring = require('querystring');
 const moment = require('moment');
 const Feels = require('feels');
 
-const port = 3000;
+const stationUrl = 'http://10.0.1.5/livedata.htm';
 const stUrl = process.env.ST_URL;
-const debug = !!process.env.DEBUG;
+const wundergroundStation = process.env.WUNDERGROUND_STATION;
+const wundergroundPassword = process.env.WUNDERGROUND_PASSWORD;
+const wundergroundUpdateUrl =
+	'http://rtupdate.wunderground.com/weatherstation/updateweatherstation.php';
 
-const app = express();
+const interval = (parseInt(process.env.INTERVAL, 10) || 60) * 1000;
+const debug = !!process.env.DEBUG;
 
 let oldData = {};
 
@@ -20,9 +24,80 @@ function isDifferent(oldData, newData, skipKeys = ['dateutc']) {
 	});
 }
 
-app.get('*', async (req, res) => {
-	const data = req.query;
+const valToNumber = val => {
+	const parsed = parseFloat(val);
 
+	if (parsed === NaN) return null;
+	return parsed;
+};
+
+const fetchFromStation = async () => {
+	const resp = await fetch(stationUrl);
+	const respData = await resp.text();
+
+	const $ = cheerio.load(respData);
+
+	const data = {
+		humidity: valToNumber($('[name=outHumi]').val()),
+		indoortempf: valToNumber($('[name=inTemp]').val()),
+		indoorhumidity: valToNumber($('[name=inHumi]').val()),
+		tempf: valToNumber($('[name=outTemp]').val()),
+		winddir: valToNumber($('[name=windir]').val()),
+		windspeedmph: valToNumber($('[name=avgwind]').val()),
+		windgustmph: valToNumber($('[name=gustspeed]').val()),
+		solarradiation: valToNumber($('[name=solarrad]').val()),
+		UV: valToNumber($('[name=uv]').val()),
+		rainin: valToNumber($('[name=rainofhourly]').val()),
+		dailyrainin: valToNumber($('[name=rainofdaily]').val()),
+		baromin: valToNumber($('[name=AbsPress]').val())
+	};
+
+	return data;
+};
+
+const updateWunderground = async rawData => {
+	const data = { ...rawData };
+
+	data.action = 'updateraw';
+	data.dateutc = moment.utc().format('YYYY-MM-DD HH:mm:ss');
+	data.ID = wundergroundStation;
+	data.PASSWORD = wundergroundPassword;
+	data.realtime = 1;
+	data.rtfreq = interval;
+
+	// Update wunderground
+	try {
+		const wuUrl = `${wundergroundUpdateUrl}?${querystring.stringify(data)}`;
+		const wuResp = await fetch(wuUrl);
+
+		const respBody = await wuResp.text();
+
+		if (wuResp.ok) {
+			console.log('WU update success');
+		} else {
+			console.warn(`WU update error (${wuResp.status})`, respBody);
+		}
+	} catch (e) {
+		console.error('WU Update failed', e);
+	}
+};
+
+const updateSmartThings = async rawData => {
+	const data = { ...rawData };
+
+	try {
+		const stResp = await fetch(stUrl, {
+			method: 'POST',
+			body: JSON.stringify(data)
+		});
+		if (debug) console.debug(JSON.stringify(data));
+		console.log('ST update success');
+	} catch (e) {
+		console.error('ST Update failed', e);
+	}
+};
+
+const handleData = async data => {
 	if (parseFloat(data.tempf) < -100 || parseFloat(data.windspeedmph) < 0) {
 		console.debug('Bad data');
 		return;
@@ -50,42 +125,19 @@ app.get('*', async (req, res) => {
 
 	oldData = { ...data };
 
-	// Update wunderground
+	await updateWunderground(data);
+	await updateSmartThings(data);
+};
+
+const run = async () => {
 	try {
-		data.dateutc = moment.utc().format('YYYY-MM-DD HH:mm:ss');
-		const wuUrl = `http://${req.hostname}${
-			req.path
-		}?${querystring.stringify(data)}`;
-		const wuResp = await fetch(wuUrl);
-
-		const respBody = await wuResp.text();
-
-		wuResp.headers.forEach((val, key) => {
-			res.set(key, val);
-		});
-		res.send(respBody);
-
-		if (wuResp.ok) {
-			console.log('WU update success');
-		} else {
-			console.warn(`WU update error (${wuResp.status})`, respBody);
-		}
+		const data = await fetchFromStation();
+		if (data) await handleData(data);
 	} catch (e) {
-		console.error('WU Update failed', e);
+		console.error(e);
 	}
 
-	// ST Update
-	try {
-		delete req.query.PASSWORD;
-		const stResp = await fetch(stUrl, {
-			method: 'POST',
-			body: JSON.stringify(req.query)
-		});
-		if (debug) console.debug(JSON.stringify(req.query));
-		console.log('ST update success');
-	} catch (e) {
-		console.error('ST Update failed', e);
-	}
-});
+	setTimeout(run, interval);
+};
 
-app.listen(port, () => console.log(`Proxy listening on port ${port}!`));
+run();
